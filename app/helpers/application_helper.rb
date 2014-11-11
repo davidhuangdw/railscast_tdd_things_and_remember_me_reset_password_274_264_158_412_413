c970c98e9f94480a974fb334051b3c61e5f98c27
DavidHdw <tossboydavid@sina.com>
2014-11-11 17:58:42 +0800

    custom form helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+
+  def input_for(column, field_method=default_field_method(column))
+    field_method = method(field_method)
+    column =column.to_sym
+    content_tag :div, class:'form-group' do
+      label_tag(column) +
+      field_method.call(column, params[column], class:'form-control')
+    end
+  end
+
+  private
+  def default_field_method(column)
+    column.to_s =~ /password/ ?
+        :password_field_tag : :text_field_tag
+  end
 end
